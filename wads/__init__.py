3a35fd7b25975ef7ccb8e27805df589bfb03c729
GitHub CI Runner <vferon@pentalog.com>
2021-03-02 19:10:33 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- wads/__init__.py
+++ wads/__init__.py
@@ -18,36 +18,36 @@ root_dir = os.path.dirname(__file__)
 root_dir_name = os.path.basename(root_dir)
 rjoin = lambda *paths: os.path.join(root_dir, *paths)
 
-data_dir = rjoin("data")
-licenses_json_path = rjoin(data_dir, "github_licenses.json")
-github_ci_tpl_path = rjoin(data_dir, "github_ci_tpl.yml")
-gitlab_ci_tpl_path = rjoin(data_dir, "gitlab_ci_tpl.yml")
+data_dir = rjoin('data')
+licenses_json_path = rjoin(data_dir, 'github_licenses.json')
+github_ci_tpl_path = rjoin(data_dir, 'github_ci_tpl.yml')
+gitlab_ci_tpl_path = rjoin(data_dir, 'gitlab_ci_tpl.yml')
 
 pkg_dir = os.path.dirname(root_dir)
 pkg_join = lambda *paths: os.path.join(pkg_dir, *paths)
 
 # TODO: Change to use ini format? (Or yaml or toml?)
-wads_configs_file = rjoin(data_dir, "wads_configs.json")
+wads_configs_file = rjoin(data_dir, 'wads_configs.json')
 try:
     wads_configs = json.load(open(wads_configs_file))
 except FileNotFoundError:
     wads_configs = {
-        "populate_dflts": {
-            "description": "There is a bit of an air of mystery around this project...",
-            "root_url": None,
-            "author": None,
-            "license": "mit",
-            "description_file": "README.md",
-            "long_description": "file:README.md",
-            "long_description_content_type": "text/markdown",
-            "keywords": None,
-            "install_requires": None,
-            "verbose": None,
-            "version": "0.0.1",
+        'populate_dflts': {
+            'description': 'There is a bit of an air of mystery around this project...',
+            'root_url': None,
+            'author': None,
+            'license': 'mit',
+            'description_file': 'README.md',
+            'long_description': 'file:README.md',
+            'long_description_content_type': 'text/markdown',
+            'keywords': None,
+            'install_requires': None,
+            'verbose': None,
+            'version': '0.0.1',
         }
     }
 
-pkg_path_names = (".gitignore", "setup.py")
+pkg_path_names = ('.gitignore', 'setup.py')
 pkg_paths = {pkg_join(name) for name in pkg_path_names}
 
 from wads.populate import populate_pkg_dir
@@ -66,5 +66,5 @@ def main():
     parser.dispatch()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
