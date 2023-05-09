a9b8b3b4798b1638b6122ccce32e5f0f5cd8334c
Andie.Phan <andie.phan@analog.com>
2023-05-08 18:16:58 -0700

    test generate_package

--- wads/tests/test_package_generation.py
+++ wads/tests/test_package_generation.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import tarfile
 import tempfile
 from pathlib import Path
 
@@ -9,7 +10,6 @@ EXAMPLE_MODULE = Path(__file__).parent / 'data' / 'example_module.py'
 
 
 def test_generate_package():
-
     with tempfile.TemporaryDirectory() as tmp_folder:
         tmp_path = Path(tmp_folder)
         output_path = tmp_path / 'output'
@@ -19,14 +19,20 @@ def test_generate_package():
             output_path=output_path,
             version='8.8.8',
         )
-        i2_whl = next((output_path / 'dist').glob('i2*-.whl'))
-        assert i2_whl, 'wheel not built from specified git repo'
 
         subprocess.check_output(
             [sys.executable, 'setup.py', 'sdist'], cwd=output_path,
         )
+        i2_whl = next((output_path / 'dist').glob('i2-*.whl'))
+        assert i2_whl, 'wheel not built from specified git repo'
         module_tar_gz = next((output_path / 'dist').glob('*.tar.gz'))
-
-        assert len(i2_whl.read_bytes()) < len(
-            module_tar_gz.read_bytes()
+        assert module_tar_gz.is_file()
+        with tarfile.open(module_tar_gz, 'r:gz') as tar:
+            tar.extractall(tmp_path / 'unpacked')
+        unpacked_dist = (
+            tmp_path / 'unpacked' / module_tar_gz.name[: -len('.tar.gz')] / 'dist'
+        )
+        unpacked_i2_whl = next(unpacked_dist.glob('i2-*.whl'))
+        assert (
+            unpacked_i2_whl.read_bytes() == i2_whl.read_bytes()
         ), '.tar.gz does not include dependency wheel'
